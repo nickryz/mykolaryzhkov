755b672455860c79d7d05b007621885c60db1d0e
nickryz <nickryz87@gmail.com>
2018-02-12 20:44:26 +0200

    v.1.1

--- dist/php/mail.php
+++ dist/php/mail.php
@@ -6,7 +6,7 @@ $sitename = "Мой сайт";
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
-$message = "Имя: $name \nEmail: $phone \nТекст: $text";
+$message = "Имя: $name \nEmail: $email \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
